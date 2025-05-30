57adb899b91c8577c7dc06ac5a4972dc20cd0af5
Vijay-tech-cmd <jadavijay555@gmail.com>
2025-05-30 22:26:56 +0530

    Update API ednpoints to use deployed backend URL

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     setError('');
     
     try {
-      const response = await fetch('http://localhost:4000/login', {
+      const response = await fetch('https://fakestorebackend-di9i.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const response = await fetch('http://localhost:4000/register', {
+      const response = await fetch('https://fakestorebackend-di9i.onrender.com/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
